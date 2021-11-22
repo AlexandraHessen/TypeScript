26fb2b20f27ce881c1b156fa30780b44f2b6a6ca
AlexandraHessen <xl_energy@mail.ru>
2021-11-22 21:50:38 +0300

    types_auto

--- A_TS0-TS1/TS080_types_auto/app.ts
+++ A_TS0-TS1/TS080_types_auto/app.ts
@@ -1,8 +1,17 @@
 
 var temperature=5;
-console.log(temperature);
+// не указать тип и указать any разные вещи
+// если не указать тип и сразу присвоить то такой и будет тип
+// если не указать тип и ничего не присвоить будет any
+// если указать any это значит что вообще всегда любой тип
+
+console.log(temperature); //5
+
+// var temperature;
+// temperature=5;
+// в этом случае temperature - any
 
 temperature=7;
-console.log(temperature);
+console.log(temperature); //7
 
 // temperature="high"; // ошибка

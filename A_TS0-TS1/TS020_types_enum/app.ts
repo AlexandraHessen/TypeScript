ac11ce12e88c4067ea480b6fb3149ec6619096b8
AlexandraHessen <xl_energy@mail.ru>
2021-11-22 21:46:05 +0300

    enum

--- A_TS0-TS1/TS020_types_enum/app.ts
+++ A_TS0-TS1/TS020_types_enum/app.ts
@@ -1,18 +1,25 @@
 enum Color { BLACK, YELLOW, RED=8, GREEN };
-
+// каждая последующая переммная увеличивается на 1
+// может оказаться что две const равны одному и тому же значению
+// новый тип Color и поэтому с большой буквы
+// BLACK, =0
+// YELLOW, =1
+// RED=8, 
+// GREEN =9
 var col1:Color=Color.BLACK;
-console.log(col1);
+console.log(col1); //0
 
-console.log(Color.YELLOW);
+console.log(Color.YELLOW); //1
 
-console.log(Color.GREEN);
+console.log(Color.GREEN); //9
 
-console.log(Color[1]);
+console.log(Color[1]); //YELLOW
 
 
 // это не enum
 
 var myStr:'First'|'Second';
+// | или
 var myStr2:5|'Second';
 
 myStr='Second';

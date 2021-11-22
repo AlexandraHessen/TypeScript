ec310fa679b0697cdb64ae674428ad6883aa3ffe
AlexandraHessen <xl_energy@mail.ru>
2021-11-22 21:49:53 +0300

    type obj

--- A_TS0-TS1/TS070_types_hash/app.ts
+++ A_TS0-TS1/TS070_types_hash/app.ts
@@ -1,9 +1,11 @@
 var friend:{fam:string,im:string,otch:string,age:number}
     ={fam:"Иванов",im:"Иван",otch:"Иванович",age:25};
-
+// только 4 ключа больше нельзя добавлять
 console.log(friend);
 
 
 var friendKey:keyof {fam:string,im:string,otch:string,age:number};
+// friendKey:keyof означает что переменная может быть только одним из ключей
 friendKey="fam";
 // friendKey="fam2"; // ошибка
+console.log(friendKey) //fam

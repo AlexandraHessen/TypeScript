27a880af1d654d2d274320299a76362b5d25099c
AlexandraHessen <xl_energy@mail.ru>
2021-11-23 21:30:52 +0300

    type

--- A_TS0-TS1/TS110_types_type/app.ts
+++ A_TS0-TS1/TS110_types_type/app.ts
@@ -1,5 +1,17 @@
 
+// type новыйтип=старыйтип;
+
+// Например:
+
+// type PersonIdent=number;
+// let ivanovId:PersonIdent=25;
+
+// type Color=number|string;
+// let danger:Color=0xFF0000;
+// let success:Color='green';
+
 type StringList=Array<string>;
+// массив содержание тоько строки
 
 var sl1:StringList=["a","b","c"];
 var sl2:StringList=sl1;

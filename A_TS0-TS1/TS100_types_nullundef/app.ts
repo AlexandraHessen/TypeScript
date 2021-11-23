c5bd0e54eac06942e7b1c652ee1e78e21feeb972
AlexandraHessen <xl_energy@mail.ru>
2021-11-23 21:30:06 +0300

    nullundef

--- A_TS0-TS1/TS100_types_nullundef/app.ts
+++ A_TS0-TS1/TS100_types_nullundef/app.ts
@@ -2,7 +2,7 @@
 var n:null=null;
 
 var u:undefined=undefined;
-
+// null и undefined можно присвоить любым переменным любого другого типа
 var varstr:string=null;
 var varnum:number=undefined;
 var varbool:boolean=null;

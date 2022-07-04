9ceff93f2d9971bf950f13389d021304805d6f4e
AlexandraHessen <xl_energy@mail.ru>
2022-07-04 21:41:21 +0300

    tuple

--- A_TS0-TS1/TS030_types_tuple/app.ts
+++ A_TS0-TS1/TS030_types_tuple/app.ts
@@ -3,3 +3,6 @@ var friend:[string,string,string,number]
 
 console.log(friend[1]); //"Иван"
 console.log(friend[3]); //25
+
+let person:[string,number]=['Иванов',29];
+console.log( person[1] ); // 29 
